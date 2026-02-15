7b2499da8ca9ae92b8d7dbdd331f4a150eb0c022
Alexsandrua <alexsanderua@yahoo.com>
2026-02-15 19:20:22 +0200

    title ?

--- src/App.js
+++ src/App.js
@@ -15,17 +15,22 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="container-fluid bg-dark text-white " data-bs-theme="dark">
-        
+
         <table className='container-fluid table table-dark'><tbody>
-         <tr><th> <Logo /> -EOM191721UNR- </th></tr> 
+          <tr><th> <Logo /> -EOM191721UNR- </th></tr>
           <tr>
-          <th>
-            <div>< Display /></div>
-          </th>
-          <th>
-            <div className='container-fluid'> <PanelAction /></div>
-          </th>
-        </tr>
+            <th>
+              <div>< Display /></div>
+            </th>
+            <th>
+              <div>
+                <div className='container-fluid'> <PanelAction /></div>
+                <div className='container-fluid text-center p-4'> 
+                  <h6 className="w-inline-block" tabIndex="0" data-toggle="tooltip" data-placement="bottom" title="0,0,0,НІ,&i,1,1,1,END : 0,0,0,АБО,1,1,1,1,&i,1,1,1,&i,END" >{"Підсказка -?- наведи"}</h6>
+                  </div>
+              </div>
+            </th>
+          </tr>
           <tr><th colSpan="2">
             <div className='container-fluid'>
               <Logo /> -Перфокарта-
